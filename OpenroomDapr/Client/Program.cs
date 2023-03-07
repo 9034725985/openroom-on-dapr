47d188998fb564280ac481ec7ef7003dc12b867a
Kushal <kushaldeveloper@gmail.com>
2023-03-07 09:21:49 -0600

    wip

--- OpenroomDapr/Client/Program.cs
+++ OpenroomDapr/Client/Program.cs
@@ -1,14 +1,24 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenroomDapr.Client;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("OpenroomDapr.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddBlazorise(options =>
+ {
+     options.Immediate = true;
+ })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("OpenroomDapr.ServerAPI"));
